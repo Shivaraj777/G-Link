9262809bfc48dfd4b1621bd2274cce29d46c8ac9
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-12-27 12:05:37 +0530

    create Chat model/schema

---
+++ server/model/chat.js
@@ -0,0 +1,30 @@
+// imports
+const mongoose = require('mongoose');
+
+// create the chat schema
+const chatSchema = new mongoose.Schema({
+    chatName: {
+        type: String,
+        trim: true
+    },
+    isGroupChat: {
+        type: Boolean,
+        default: false
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    groupAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+// create the model
+const Chat = mongoose.model('Chat', chatSchema);
+
+// export the model
+module.exports = Chat;
