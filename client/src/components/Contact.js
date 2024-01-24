c6a17e20420559761fc36d06775cbf4cf0b6f74d
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-01-24 20:39:21 +0530

    Contact section - CSS update

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,19 +6,19 @@ function Contact() {
   return (
     <StyledContact id='contact'>
       <div className='custom-container'>
-        <div className='warpper w-full flex lg:flex-grow flex-col lg:space-x-[30px] justify-center items-center'>
+        <div className='wrapper w-full flex lg:flex-row flex-col lg:space-x-[30px] justify-center items-center'>
           <div className='feedback-content lg:w-1/2 w-full'>
             {/* section-header */}
-            <div className='title'>
+            <div className='title' data-aos='fade-right'>
               <h1>Feel free to drop me your feedback</h1>
             </div>
           </div>
 
           {/* feedback section */}
-          <div className='feedback-content'>
-            {/* <div className='shape-container absolute'>
+          <div className='feedback-content' data-aos='fade-left'>
+            <div className='shape-container absolute'>
               <img src='/images/circle-dot.png' alt='circle-dot-shape' loading='lazy' />
-            </div> */}
+            </div>
 
             {/* feedback form */}
             <div className='feedback-form flex-1 bg-white sm:p-10 py-5 px-5'>
@@ -92,6 +92,126 @@ function Contact() {
 }
 
 const StyledContact = styled.section`
+  position: relative;
+  max-width: 100vw;
+  height: 100%;
+  background-color: #0c1631;
+  overflow: hidden;
+  padding: 9rem 0;
+
+  .custom-container{
+    position: relative;
+    height: 100%;
+    padding: 6rem 5rem;
+    z-index: 5;
+    .wrapper{
+      .feedback-content{
+        height: 100%;
+        position: relative;
+        margin-right: 20px;
+        padding-right: 20px;
+
+        .title{
+          text-align: center;
+          h1{
+            font-size: 3rem;
+            font-weight: 700;
+            line-height: 1.3;
+            color: ${({theme}) => theme.colors.white}
+          }
+        }
+
+        .feedback-form{
+          border-radius: 10px;
+          input, textarea{
+            width: 100%;
+            padding: 0.5rem 0.75rem;
+            background-color: #f6f6f9;
+            color: ${({ theme }) => theme.colors.black};
+          }
+
+          .title{
+            text-align: center;
+            h2{
+              font-size: 2.5rem;
+              font-weight: 700;
+              line-height: 1.3;
+              color: ${({theme}) => theme.colors.black};
+            }
+          }
+        }
+
+        .shape-container{
+          top: -50px;
+          right: -60px;
+          img{
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+
+  .shapes{
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+
+    .svg-1{
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0%;
+      left: 0%;
+      svg{
+        fill: ${({ theme }) => theme.colors.bg2.secondary};
+      }
+    }
+
+    .svg-2,
+    .svg-3,
+    .svg-4{
+      width: 100%;
+      height: 100%;
+      position: absolute;
+    }
+
+    .svg-2{
+      top: 80%;
+      left: 0%;
+    }
+
+    .svg-3{
+      top: 75%;
+      left: 0%;
+    }
+
+    .svg-4{
+      top: 70%;
+      left: 0%;
+    }
+  }
+
+  // media queries
+  @media only screen and (max-width: 992px) {
+    h1{
+      font-size: 2rem;
+      margin-bottom: 2rem;
+    }
+
+    .custom-container .wrapper{
+      .feedback-content{
+        margin: 0;
+        padding: 0;
+      }
+    }
+
+    .feedback-content{
+      margin-bottom: 2.5rem;
+    }
+  }
 `;
 
 export default Contact;
