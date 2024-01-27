158dc1e04be87d14a5edc2e4d4471faf5f6fbd52
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-01-27 12:30:21 +0530

    access  redux store using useSelector hook

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,11 @@ import HomePage from './pages/HomePage';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
+  // access store state using useSelector hook
+  const { darkThemeEnabled } = useSelector((state) => state.theme);
 
   // initialize AOS
   useEffect(() => {
@@ -16,6 +19,8 @@ function App() {
     });
   }, []);
 
+
+  // app light theme
   const lightTheme = {
     colors: {
       heading: "rgb(24 24 29)",
@@ -60,9 +65,57 @@ function App() {
     }
   };
 
+
+  // app dark theme
+  const darkTheme = {
+    colors: {
+      heading: "rgb(255, 255, 255)",
+      heading2: "rgb(24 24 29)",
+      white: "#ffffff",
+      black: "#000000",
+      cyan: "#1ca9fe",
+      green: "#4eac6d",
+      danger: "#ff4e2b",
+      light: "#223645",
+
+      text: {
+        primary: "#212529",
+        secondary: "#8f9198",
+      },
+
+      bg: {
+        black: "#000000",
+        primary: "#262626",
+        secondary: "#2e2e2e",
+      },
+      border2: {
+        primary: "#FFFFFF26",
+      },
+
+      bg2: {
+        primary: "#0c1631",
+        secondary: "#0e1b38",
+      },
+      boxShadow: {
+        primary: "rgba(1, 201 ,245, 0.4)",
+      },
+
+      hr: "#ffffff",
+      border: "65, 66, 72",
+      img_border: "31, 41, 55",
+      gradient: "linear-gradient(145deg,#1ca9fe,#1c6ee9);",
+    },
+
+    media: {
+      mobile: "800px",
+      tab: "998px",
+    },
+  };
+
+
   return (
     <div className="App">
-      <ThemeProvider theme={lightTheme}>
+      <ThemeProvider theme={darkThemeEnabled ? darkTheme : lightTheme}>
         <HomePage />
       </ThemeProvider>
     </div>
