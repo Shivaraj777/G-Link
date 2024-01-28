acfba0369b9a36e31d1700c0e520c4d92c397d2b
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-01-28 16:40:12 +0530

    create Auth page wrapper

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DefaultLayoutHoc from '../layout/DefaultLayoutHoc';
+
+function AuthPage() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default DefaultLayoutHoc(AuthPage);
