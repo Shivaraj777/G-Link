176a28d97ce43dff7173f942dad943bd85ff1e30
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-02-08 20:50:05 +0530

    Server update: updating users API route

--- server/routes/api/v1/user.js
+++ server/routes/api/v1/user.js
@@ -17,7 +17,7 @@ router.post('/create-session', usersAPI.createSession);
 router.get('/search-users/:search', passport.authenticate('jwt', {session: false}), usersAPI.searchUsers);
 router.get('/get-user-detail', passport.authenticate('jwt', {session: false}), usersAPI.getUser);
 router.put('/update-profile', passport.authenticate('jwt', {session: false}), usersAPI.updateProfile);
-router.get('/resend/verification-email', usersAPI.resendVerificationEmail);
+router.post('/resend/verification-email', usersAPI.resendVerificationEmail);
 router.patch('/verify-email', usersAPI.verifyEmail);
 router.get('/forgot-password', usersAPI.forgotPassword);
 router.patch('/reset-password', usersAPI.resetPassword);
