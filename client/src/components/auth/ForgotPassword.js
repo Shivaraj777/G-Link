81f9c18b462a6876e7c11152f54bc61fd4ccbe1d
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-02-10 13:41:42 +0530

    define logic for Forgot password page

--- client/src/components/auth/ForgotPassword.js
+++ client/src/components/auth/ForgotPassword.js
@@ -1,16 +1,67 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { clearAuthStore, forgotPasswordScenario } from '../../redux/auth/auth.action';
+import { useNavigate } from 'react-router-dom';
 
 function ForgotPassword() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [message, setMessage] = useState('');
+
+  const auth = useSelector((state) => state.auth);
 
   // state to manage form data
   const [userData, setUserData] = useState({ email: '' });
-  console.log(userData);
 
   // handle change in form data
   const handleChange = (e) => {
     setUserData((prevState) => ({...prevState, [e.target.name]: e.target.value }));
   }
+
+
+  // display page status
+  useEffect(() => {
+    if(auth.message){
+      setMessage(auth.message);
+      if(!auth.status){
+        console.log(auth.message);
+      }
+    }else{
+      console.log('Password Reset mail sent successfully');
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth.message]);
+
+
+  // send password reset email
+  const sendPasswordResetEmail = () => {
+    if(userData.email === '' || userData.email === null){
+      console.log('Please enter a valid email');
+      return;
+    }
+
+    dispatch(forgotPasswordScenario(userData.email));
+    setUserData({ email: '' });
+  }
+
+
+  // handle navigate to home page
+  const navigateToHome = () => {
+    dispatch(clearAuthStore());
+    setUserData({ email: '' });
+    navigate('/');
+  }
+
+
+  // handle resetting forgot pasword page to resend email
+  const handlePageReset = () => {
+    dispatch(clearAuthStore());
+    setMessage('');
+    setUserData({ email: '' });
+  }
+
 
   return (
     <Wrapper>
@@ -20,13 +71,19 @@ function ForgotPassword() {
             Forgot Password
           </h2>
 
-          {false ? (
+          {auth.success ? (
             <>
-              <p className='text-center'>Message</p>
-              <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
+              <p className='text-center'>{message}</p>
+              <button 
+                className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
+                onClick={navigateToHome}
+              >
                 Home
               </button>
-              <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
+              <button 
+                className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
+                onClick={handlePageReset}
+              >
                 Resend Password Reset Link
               </button>
             </>) : (
@@ -52,7 +109,10 @@ function ForgotPassword() {
                     onChange={handleChange}
                   />
                 </div>
-                <button className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                <button 
+                  className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                  onClick={sendPasswordResetEmail}
+                >
                   Reset Password Link
                 </button>
               </div>
