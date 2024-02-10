cf67f7904d5207c7454cd7390bbd1ce0670efbc0
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-02-10 17:52:16 +0530

    handle logic for reset password page

--- client/src/components/auth/ResetPassword.js
+++ client/src/components/auth/ResetPassword.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { clearAuthStore, resetUserPassword } from '../../redux/auth/auth.action';
+import { toast } from 'react-toastify';
 
 function ResetPassword() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { token } = useParams();
+  const [message, setMessage] = useState('');
+
+  const auth = useSelector((state) => state.auth);
+
   // state to manage form data
   const [userData, setUserData] = useState({
     token: '',
@@ -14,6 +25,75 @@ function ResetPassword() {
     setUserData((prevState) => ({...prevState, [e.target.name]: e.target.value }));
   }
 
+
+  // get token from URI
+  useEffect(() => {
+    if(token !== null){
+      setUserData((prevState) => ({ ...prevState, token: token }));
+    }
+  }, [token]);
+
+
+  // handle page/reset password status
+  useEffect(() => {
+    if(auth.message){
+      setMessage(auth.message);
+
+      if(!auth.success){
+        toast.error(auth.message, {
+          autoClose: 2000
+        });
+      }else{
+        toast.success(auth.message, {
+          autoClose: 2000
+        });
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth.message]);
+
+
+  // handle reset user password
+  const handleResetUserPassword = () => {
+    const newPassword = userData.newPassword;
+    const confirmPassword = userData.confirmPassword;
+
+    if((newPassword === null || newPassword === '') && (confirmPassword === null || confirmPassword === '')){
+      toast.warn('Please fill both New Password and Confirm Password field', {
+        autoClose: 2000
+      });
+      return 
+    }
+
+    if(newPassword !== confirmPassword){
+      toast.warn('Password and Confirm Password does not match', {
+        autoClose: 2000
+      });
+      return;
+    }
+
+    dispatch(resetUserPassword(userData));
+    setUserData((prevState) => ({ ...prevState, newPassword: '', confirmPassword: ''}));
+  }
+
+
+  // handle navigate to home page
+  const navigateToHome = () => {
+    dispatch(clearAuthStore());
+    setUserData((prevState) => ({ ...prevState, newPassword: '', confirmPassword: ''}));
+    navigate('/');
+  }
+
+
+  // handle page reset
+  const handlePageReset = () => {
+    dispatch(clearAuthStore());
+    setUserData((prevState) => ({ ...prevState, newPassword: '', confirmPassword: ''}));
+    navigate("/forgot-password");
+  }
+
+
   return (
     <Wrapper>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
@@ -24,11 +104,17 @@ function ResetPassword() {
 
           {false ? (
             <>
-              <p className='text-center'>Message</p>
-              <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
+              <p className='text-center'>{message}</p>
+              <button 
+                className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
+                onClick={navigateToHome}
+              >
                 Home
               </button>
-              <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
+              <button 
+                className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
+                onClick={handlePageReset}
+              >
                 Resend Password Reset Link
               </button>
             </>) : (
@@ -72,7 +158,10 @@ function ResetPassword() {
                     onChange={handleChange}
                   />
                 </div>
-                <button className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                <button 
+                  className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                  onClick={handleResetUserPassword}
+                >
                   Reset Password
                 </button>
               </div>
