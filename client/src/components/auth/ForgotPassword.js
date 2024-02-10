df93535602019fb27837a803d491b32dc17702fc
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-02-10 12:55:54 +0530

    Forgot password page: handle change in form data

--- client/src/components/auth/ForgotPassword.js
+++ client/src/components/auth/ForgotPassword.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function ForgotPassword() {
+
+  // state to manage form data
+  const [userData, setUserData] = useState({ email: '' });
+  console.log(userData);
+
+  // handle change in form data
+  const handleChange = (e) => {
+    setUserData((prevState) => ({...prevState, [e.target.name]: e.target.value }));
+  }
+
   return (
     <Wrapper>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
@@ -38,6 +48,8 @@ function ForgotPassword() {
                     className='input input-md h-11 sm:text-sm rounded-lg w-full p-2.5 dark:placeholder-gray-400'
                     placeholder='janedoe@gmail.com'
                     required=''
+                    value={userData.email}
+                    onChange={handleChange}
                   />
                 </div>
                 <button className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
