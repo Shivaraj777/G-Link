077937835e9c655f65ce2427ff9eeb71d5488a4e
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-02-26 23:57:20 +0530

    chat helper func to check for user message

--- client/src/helperFunction/chat.helper.js
+++ client/src/helperFunction/chat.helper.js
@@ -16,3 +16,18 @@ export const getSenderPic = (loggedUser, users) => {
 
   return users[0]._id === loggedUser._id ? users[1].profile : users[0].profile;
 };
+
+
+
+// check whether message is sent by logged-in user
+export const isMyMessage = (loggedUser, message) => {
+  if(!message.sender || !loggedUser){
+    return;
+  }
+
+  if(loggedUser._id === message.sender._id){
+    return true;
+  }
+
+  return false;
+};
