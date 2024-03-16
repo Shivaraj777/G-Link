cfd2c273fbe97001ad4d780ea7fe5262464c56a6
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-03-16 13:51:43 +0530

    deployment update - 16/3/24

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,8 +2,8 @@
 
 // site FQDN
 // const API_ROOT = 'http://localhost:8000/api/v1';
-// const API_ROOT = process.env.API_ROOT_URL;
-const API_ROOT = 'https://g-link-api.onrender.com/api/v1';
+const API_ROOT = process.env.REACT_APP_API_ROOT_URL;
+// const API_ROOT = 'https://g-link-api.onrender.com/api/v1';
 
 // api urls
 export const API_URLs = {
@@ -29,6 +29,6 @@ export const API_URLs = {
 export const ACCESS_TOKEN_KEY = '__glink_token__';
 
 // export const SERVER_ACCESS_BASE_URL = 'http://localhost:8080';
-// export const SERVER_ACCESS_BASE_URL = process.env.CHAT_SERVER_FQDN;
-export const SERVER_ACCESS_BASE_URL = 'https://g-link-api.onrender.com:8080';
+export const SERVER_ACCESS_BASE_URL = process.env.REACT_APP_CHAT_SERVER_FQDN;
+// export const SERVER_ACCESS_BASE_URL = 'https://g-link-api.onrender.com:8080';
 
